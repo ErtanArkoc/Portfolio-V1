92e9e082c3af7f32dc453c3c6fbf66b0baf80fd9
ErtanArkoc <83455198+ErtanArkoc@users.noreply.github.com>
2021-05-10 17:11:54 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,3 +17,8 @@ $("#body").on("mousedown", function (e) {
     $(this).removeClass("mouseDown");
 });
 
+const logo = document.querySelectorAll("#logo path");
+
+for (let i = 0; i<logo.length; i++) {
+    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+}
